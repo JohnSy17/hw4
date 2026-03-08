93cf461a9698a075aa95e44188fed0b95c1c5480
JohnSy17 <85195933+JohnSy17@users.noreply.github.com>
2026-03-08 22:54:21 +0000

    Update the users controller and users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   def new
+    # render the new user page
   end
 
   def create
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"]) # store encrypted valuye into table
     @user.save
     redirect_to "/"
   end
