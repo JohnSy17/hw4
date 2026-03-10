a634c6ca6703014887172a02b8e279861c6e2796
JohnSy17 <85195933+JohnSy17@users.noreply.github.com>
2026-03-10 18:47:04 +0000

    Edit places controller, entries controller, and update button formats

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,11 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] , "user_id" => session["user_id"]})
+    if session["user_id"].nil?
+      @entries =[]
+    else
+      @entries = Entry.where({ "place_id" => @place["id"] , "user_id" => session["user_id"]})
+    end
   end
 
   def new
